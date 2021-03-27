16d243f311e9ecd8aa040f52f4f1290d918fbd8e
Nabila Sinta <68362834+nabilasinta0@users.noreply.github.com>
2021-03-27 11:54:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,184 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>My Portofolio</title>
+</head>
+<body>
+    <!-- Section Home -->
+    <section class="page-section home" id="home">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #8b0000;">
+    <div class="container">
+        <a class="navbar-brand" href="#">Halaman Admin</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link active rounded js-scroll-trigger" aria-current="page" href="#home">Home</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link rounded js-scroll-trigger" href="#about">About</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link rounded js-scroll-trigger" href="#galery">Galery</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link rounded js-scroll-trigger" href="#kontak">Contact</a></li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 290"><path fill="#8b0000" fill-opacity="1" d="M0,128L48,122.7C96,117,192,107,288,117.3C384,128,480,160,576,144C672,128,768,64,864,69.3C960,75,1056,149,1152,176C1248,203,1344,181,1392,170.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+    <!-- Akhir Section Home -->
+    
+    <!-- Jumbotron -->
+    <div class="jumbotron text-center pb-5">
+        <img src="pro.jpg" alt=""  srcset="" class="rounded-circle img-thumbnail pt-0" width="220px"> 
+        <h1 class="display-5">NABILA SINTA ISMAWATI</h1>
+        <p class="lead">Student | Designer</p>
+    </div>
+    </section>
+    <!-- Akhir Jumbotron -->
+
+    <!-- Section About Me -->
+    <section class="page-section mb-0" id="about">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 250"><path fill="#8b0000" fill-opacity="1" d="M0,128L48,122.7C96,117,192,107,288,117.3C384,128,480,160,576,144C672,128,768,64,864,69.3C960,75,1056,149,1152,176C1248,203,1344,181,1392,170.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col">
+            <h2 class="text-center text-uppercase">About Me</h2>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center mt-4">
+    <div class="col-sm-5">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, veritatis adipisci placeat itaque esse vero voluptate fuga in cumque maiores ipsum inventore officiis unde. Sit neque error labore pariatur dolores temporibus explicabo inventore aliquam nostrum voluptatibus. Quaerat laudantium natus provident facere veritatis harum! Dignissimos doloremque libero nemo nihil, minus fuga.
+    </div>
+    <div class="col-sm-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, in deleniti voluptas doloribus, eaque veritatis alias cum quod, similique sint natus nulla accusantium amet esse itaque minus earum tenetur. Ducimus fugit nobis a libero aliquid iste provident debitis labore quam voluptatum, doloribus, porro modi ullam. Repellendus voluptatum obcaecati provident numquam sint voluptates.
+    </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 290"><path fill="#8b0000" fill-opacity="1" d="M0,256L48,240C96,224,192,192,288,192C384,192,480,224,576,202.7C672,181,768,107,864,106.7C960,107,1056,181,1152,213.3C1248,245,1344,235,1392,229.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+    <!-- Akhir Section About Me -->
+
+    <!-- Section Galery -->
+    <section class="page-section" id="galery">
+    <div class="container"><br><br>
+        <div class="row text-center m-1 p-1">
+            <div class="col">
+            <h2 class="text-center text-uppercase mt-1">Galery</h2>
+            </div>
+            </div>
+    <div class="row text-center m-1 p-1 justify-content-around">
+        <div class="col-md-4 1h-base p-3">
+            <div class="card">
+            <img src="img/gambarsatu.jpg" class="card-img-top" alt="gambar 1" />
+            <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit libero odit aspernatur!</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 1h-base p-3">
+        <div class="card">
+        <img src="img/gambarsatu.jpg" class="card-img-top" alt="gambar 1" />
+        <div class="card-body">
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit libero odit aspernatur!</p>
+        </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 1h-base p-3">
+    <div class="card">
+    <img src="img/gambarsatu.jpg" class="card-img-top" alt="gambar 1" />
+    <div class="card-body">
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit libero odit aspernatur!</p>
+    </div>
+    </div>
+    </div>
+
+    <div class="col-md-4 1h-base p-3">
+        <div class="card">
+        <img src="img/gambarsatu.jpg" class="card-img-top" alt="gambar 1" />
+        <div class="card-body">
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit libero odit aspernatur!</p>
+        </div>
+        </div>
+        </div>
+
+        <div class="col-md-4 1h-base p-3">
+            <div class="card">
+            <img src="img/gambarsatu.jpg" class="card-img-top" alt="gambar 1" />
+            <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit libero odit aspernatur!</p>
+            </div>
+            </div>
+            </div>
+
+            <div class="col-md-4 1h-base p-3">
+                <div class="card" >
+                <img src="img/gambarsatu.jpg" class="card-img-top" alt="gambar 1" />
+                <div class="card-body">
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit libero odit aspernatur!</p>
+                </div>
+                </div>
+                </div>
+        </div>
+        </div>
+        </section>
+    <!-- Akhir Section Galery -->
+
+    <!-- Section Kontak -->
+    <section class="page-section" id="kontak">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200"><path fill="#8b0000" fill-opacity="1" d="M0,128L48,122.7C96,117,192,107,288,117.3C384,128,480,160,576,144C672,128,768,64,864,69.3C960,75,1056,149,1152,176C1248,203,1344,181,1392,170.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+        <div class="container">
+            <div class="row text-center mb-4">
+                <div class="col pt-4">
+                    <h2 class="text-center text-uppercase mt-1">Contact</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <form>
+                        <div div class="mb-3">
+                            <label for="nama" class="form-label">Nama Lengkap</label>
+                            <input type="text" class="form-control" id="nama" aria-describedby="nama">
+                            </div>
+                        <div div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="pesan" class="form-label">Pesan</label>
+                                    <textarea class="form-control" id="pesan" rows="5"></textarea>
+                                </div>
+                        <button type="submit" class="btn text-white mb-5" style="background-color: #8b0000;">Kirim</button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+        </section>
+<!-- Akhir Section Kontak -->
+
+<!-- Copyright Section-->
+<div class="copyright py-4 text-center text-white" style="background-color: #8b0000;">
+    <div class="container" style="background-color: #8b0000;"><small>Copyright © NabilaSinta 2021</small></div>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+</body>
+</html>
